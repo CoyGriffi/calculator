e654b922fbfffeaf7fda2954a9624ed2f53ceebb
coygriffi <coygriffi@gmail.com>
2021-10-26 18:41:16 -0700

    add code to js file that calculates addition multiplication division and subtraction
    Co-authored-by: Matthew Banse-Fay <matthew.bansefay@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function sub(number1, number2) {
+  return number1 - number2;
+}
+
+function div(number1, number2) {
+  return number1 / number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+
+
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+const result = add(number1, number2);
+alert(result);
+
