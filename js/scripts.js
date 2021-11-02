f3410ac2dc79f80ff36512e670f7fd03916bc90d
coygriffi <coygriffi@gmail.com>
2021-11-01 20:06:54 -0700

    add seperate outputs for each funtion

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,36 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output3").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("#output4").text(result);
   });
 });
